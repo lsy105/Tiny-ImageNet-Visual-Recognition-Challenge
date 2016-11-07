a428ed436a4aca3af9c8ce774f468ed3441cf73a
lsy105 <liusy105@gmail.com>
2016-11-07 04:38:51 +0000

    Done

--- layers.py
+++ layers.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 from util import *
 
 def Conv1x1(x, out_C, training_mode):
+      """ 
+      conv layer with 1x1 filter 
+
+      Args:
+            x: input data
+            out_C: output channel
+            training_mode: if it is in training
+
+      Returns:
+            conv_out: return the result of 1x1 conv layer
+      """
+
       #get dimensions of x
       x_size, x_H, x_W, x_C = x.get_shape()
       x_H, x_W, x_C = int(x_H), int(x_W), int(x_C)
@@ -21,6 +33,18 @@ def Conv1x1(x, out_C, training_mode):
 
 
 def ConvNormReluForward(x, out_C, training_mode):
+      """
+      build pattern of conv layer->normalization->Relu 
+      filter size is 3x3
+      
+      Args:
+            x: input data
+            out_C: output channel
+            training_mode: if it is in training
+
+      Returns:
+            return output of this pattern
+      """
       #get dimensions of x 
       x_size, x_H, x_W, x_C = x.get_shape()
       x_H, x_W, x_C = int(x_H), int(x_W), int(x_C)     
@@ -40,27 +64,20 @@ def ConvNormReluForward(x, out_C, training_mode):
       return conv_out
 
 
-def Conv7x7NormReluForward(x, out_C, training_mode):
-      #get dimensions of x 
-      x_size, x_H, x_W, x_C = x.get_shape()
-      x_H, x_W, x_C = int(x_H), int(x_W), int(x_C)
-
-      #create weight and bias
-      W = InitWeights([7, 7, x_C, out_C], name='AW')
-      b = InitBias([out_C], name='b')
-
-      #conv batch and relu
-      conv_out = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-      conv_out += b
-      conv_out = tf.contrib.layers.batch_norm(conv_out, decay=0.99, center=True,
-                                              scale=True, epsilon=1e-10,
-                                              updates_collections=None,
-                                              is_training=training_mode)
-      conv_out = tf.nn.relu(conv_out)
-      return conv_out
-
-
 def ConvNormForward(x, out_C, training_mode):
+      """
+      build pattern of conv layer->normalization
+      filter size is 3x3
+      
+      Args:
+            x: input data
+            out_C: output channel
+            training_mode: if it is in training
+
+      Returns:
+            return output of this pattern
+      """
+
       #get dimensions of x 
       x_size, x_H, x_W, x_C = x.get_shape()
       x_H, x_W, x_C = int(x_H), int(x_W), int(x_C)
@@ -80,6 +97,19 @@ def ConvNormForward(x, out_C, training_mode):
 
 
 def FCLayer(x, out_size, keep_prob, is_output_layer=0, training_mode=1):
+      """
+      build FC layer
+      
+      Args:
+            x: input data
+            out_size: output channel
+            keep_prob: keep rate which is 1 - dropout rate
+            is_output_layer: if it is output layer
+            training_mode: if it is in training
+
+      Returns:
+            return output of FC layer
+      """
       x_row, x_col = x.get_shape()
       W = InitWeights([x_col, out_size], name='AW')
       b = InitBias([out_size], name='b')
